2d175ab6b4777a56776fe0338457dd112b3ce4e1
Hana Amirah Natasya <165010989+hanaamirahn@users.noreply.github.com>
2024-10-04 23:13:02 +0700

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1xVfJ4R7Ml7ZoB1da6ht8WOxhTBFHYsf0
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
